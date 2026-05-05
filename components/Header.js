5acbf4fbbf8bfd1970b372f8e2c392c33160ee94
BRGOKA <bgoudjal3@gmail.com>
2026-05-05 19:58:07 +0100

    first commit

---
+++ components/Header.js
@@ -0,0 +1,20 @@
+// import { SearchPosts } from "./SearchPosts";
+// import { Results } from "./Results";
+// import { useContext } from "react";
+
+// export function Header() {
+//   const { onClearPosts } = useContext(PostContext);
+//   console.log(onClearPosts);
+//   return (
+//     <header>
+//       <h1>
+//         <span>⚛️</span>The Atomic Blog
+//       </h1>
+//       <div>
+//         <Results />
+//         <SearchPosts />
+//         <button onClick={onClearPosts}>Clear posts</button>
+//       </div>
+//     </header>
+//   );
+// }
